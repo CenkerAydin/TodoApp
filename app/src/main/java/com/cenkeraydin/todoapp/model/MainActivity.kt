bf7312b0791c820ca1941d9dfe0fb9c267e18ab9
Cenker Aydın <20210808002@ogr.akdeniz.edu.tr>
2023-12-27 16:22:24 +0300

    inital

--- app/src/main/java/com/cenkeraydin/todoapp/model/MainActivity.kt
+++ app/src/main/java/com/cenkeraydin/todoapp/model/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.cenkeraydin.todoapp
+package com.cenkeraydin.todoapp.model
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cenkeraydin.todoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
