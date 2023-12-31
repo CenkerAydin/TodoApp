fb53ceeffdb6e535ac286ed426edaf8bfe507ff6
Cenker Aydın <20210808002@ogr.akdeniz.edu.tr>
2023-12-31 14:42:26 +0300

    Update TodoAdapter.kt

--- app/src/main/java/com/cenkeraydin/todoapp/Adapter/TodoAdapter.kt
+++ app/src/main/java/com/cenkeraydin/todoapp/Adapter/TodoAdapter.kt
@@ -1,29 +1,36 @@
 package com.cenkeraydin.todoapp.Adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.AsyncListDiffer
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.cenkeraydin.todoapp.databinding.ItemTodoBinding
 import com.cenkeraydin.todoapp.model.Todo
 
-class TodoAdapter(private val todosList:ArrayList<Todo>): RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+class TodoAdapter(private val todoList:ArrayList<Todo>, private val listener:Listener): RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+    interface Listener{
+        fun onItemClick(todoModel:Todo)
+    }
+    private val colors:Array<String> = arrayOf("#e86343","#f5dcd1","#ffa07a","#dec158","#e44803","#949587","#195765","#03363e")
 
-    inner class TodoViewHolder(val binding:ItemTodoBinding) : RecyclerView.ViewHolder(binding.root)
+    class TodoViewHolder(val binding:ItemTodoBinding) : RecyclerView.ViewHolder(binding.root) {
 
-    override fun getItemCount() =todosList.size
+        fun bind(todoModel: Todo,colors:Array<String>,position:Int, listener: Listener){
+            itemView.setOnClickListener{
+                listener.onItemClick(todoModel)
+            }
+            itemView.setBackgroundColor(Color.parseColor(colors[position % 8]))
+            binding.tvTitle.text=todoModel.todo
+            binding.cbDone.isChecked=todoModel.completed
+        }
+    }
+    override fun getItemCount() =todoList.size
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
         return TodoViewHolder(ItemTodoBinding.inflate( LayoutInflater.from(parent.context),parent,false))
     }
 
     override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
-        holder.binding.apply {
-            val todo=todosList[position]
-            tvTitle.text=todo.todo
-            cbDone.isChecked=todo.completed
-
+       holder.bind(todoList[position],colors, position, listener)
         }
     }
 }
