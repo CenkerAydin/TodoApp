279ad169baec243ce1e8f6261afd995072dfd612
Cenker Aydın <20210808002@ogr.akdeniz.edu.tr>
2024-01-01 13:49:22 +0300

    Update TodoAPI.kt

--- app/src/main/java/com/cenkeraydin/todoapp/service/TodoAPI.kt
+++ app/src/main/java/com/cenkeraydin/todoapp/service/TodoAPI.kt
@@ -1,16 +1,15 @@
 package com.cenkeraydin.todoapp.service
 
 import com.cenkeraydin.todoapp.model.Todo
+import retrofit2.Call
 import retrofit2.Response
-import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+
 
 interface TodoAPI {
 
     @GET("/todos")
-    suspend  fun getTodos(@Query("key") key:String):Response<List<Todo>>
+    fun getTodos(): Call<List<Todo>>
 
 
 }
